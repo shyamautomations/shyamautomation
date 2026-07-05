68b301c0ad2765992e39b2e1dc8cf6e2512366eb
shyamautomations <www.shyamautomation@gmail.com>
2026-07-05 15:10:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+window.addEventListener("load", () => {
+    document.body.style.opacity = "1";
+});
 
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
